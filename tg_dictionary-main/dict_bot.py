b573b0302a926bdcbec96eaab98bc90dc27ea3ab
olya nizevich <164992892+olya-nzv@users.noreply.github.com>
2024-04-18 12:44:39 +0400

    Add files via upload

---
+++ tg_dictionary-main/dict_bot.py
@@ -0,0 +1,54 @@
+from telebot import TeleBot, types
+
+bot = TeleBot(token='7180692340:AAHGyart816DhdMVKkdeJpfUEmFgspBB5Wc', parse_mode='html') 
+
+DEFINITOINS = {
+    'sopranos': 'Genetic predispositions are only that: predispositions',
+    'friends': 'Joey does not share food!',
+    'how i met your mother': 'whenever im sad, i stop being sad and be awesome instead',
+    'daria': 'I do not have low self-esteem. I have low esteem for everyone else',
+    'office': 'I am not superstitious, but I am a little stitious',
+    'house': 'Everybody lies',
+    'simpsons': 'Do h!',
+    'charmed': 'Every family has its demons',
+    'the big bang theory': 'That is My Spot',
+    'scrubs': 'Who is got two thumbs and does not give a crap? Bob Kelso',
+    'family guy': 'Lets go drink until we can not feel feelings anymore',
+}
+
+@bot.message_handler(commands=['start'])
+def start_command_handler(message: types.Message):
+    bot.send_message(
+        chat_id= message.chat.id, 
+        text='Hey! Enter the series you like, for example, Friends',
+    )
+
+@bot.message_handler()
+def message_handler(message: types.Message):
+    definition = DEFINITOINS.get(
+        message.text.lower(), 
+    )
+ 
+    if definition is None:
+       
+        bot.send_message(
+            chat_id=message.chat.id,
+            text='Sorry. I have not quoted this series yet',
+        )
+        return
+    
+    bot.send_message(
+        chat_id=message.chat.id,
+        text=f'Определение:\n<code>{definition}</code>',
+    )
+
+    bot.send_message(
+        chat_id=message.chat.id,
+        text=f'Come on next',
+    )
+
+def main():
+    bot.infinity_polling()
+
+if __name__ == '__main__':
+    main()
